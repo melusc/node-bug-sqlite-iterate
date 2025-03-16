727deb19b1ca36989da48edd91bda57a12b8b916
Luca Schnellmann <oss@lusc.ch>
2025-03-16 10:49:46 +0100

    Prettify

--- sqlite-iterate.js
+++ sqlite-iterate.js
@@ -1,31 +1,28 @@
-import {DatabaseSync} from 'node:sqlite';
+import { DatabaseSync } from "node:sqlite";
 
-const database = new DatabaseSync(':memory:');
+const database = new DatabaseSync(":memory:");
 
 database.exec(`
 	CREATE TABLE users (
 		user_id  INTEGER PRIMARY KEY AUTOINCREMENT,
 		name     TEXT NOT NULL UNIQUE
 	);
-`)
+`);
 
 for (let index = 0; index < 1e3; ++index) {
-	database.prepare(
-		`INSERT INTO users (name) VALUES (:name)`,
-	).run({
+	database.prepare(`INSERT INTO users (name) VALUES (:name);`).run({
 		name: Math.random().toString(36).slice(2),
 	});
 }
 
 // Double check all 1000 rows are inserted
-console.log(database.prepare('SELECT count(*) as row_count FROM users;').get());
-
+console.log(database.prepare("SELECT count(*) as row_count FROM users;").get());
 
 // This doesn't fail so it is iterators per se
 // const iter = [...database.prepare('SELECT * from users;').iterate()][Symbol.iterator]();
-const iter = database.prepare('SELECT * from users;').iterate();
+const iter = database.prepare("SELECT * from users;").iterate();
 
-while(true) {
+while (true) {
 	const next = iter.next();
 	if (next.done) {
 		break;
